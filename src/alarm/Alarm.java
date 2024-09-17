8b144272bf2dd590a829536ec83dd33f7fc698c6
sumanth988 <sumanthbandaram@gmail.com>
2024-09-17 21:18:03 +0530

    auto formatted the code

--- src/alarm/Alarm.java
+++ src/alarm/Alarm.java
@@ -5,60 +5,59 @@ import java.util.Queue;
 
 public class Alarm {
 	Queue<Long> q;
-	public Alarm () {
+
+	public Alarm() {
 		q = new PriorityQueue<Long>();
 		init();
 	}
-	private void init() {
-		Runnable r = ()->{
-			while(true) {
-			  	synchronized (q) {
-			  		// If q is empty wait for an insertion
-				  if(q.isEmpty()) {
-					  try {
-						  System.out.println("waiting for insertion");
-						  q.wait();
-					  } catch (InterruptedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					  }
-				  }
-				  // if q has a element check if the alarm time has come
-				  if(q.peek()-System.currentTimeMillis()<=0) {
-					  long t = q.poll();
-					  System.out.println("Current Time: "+ System.currentTimeMillis()+"Scheduled Time: "+t);
-				  }
-				  // if alarm time is not passed put a wait on the top element
-				  // Note: I have added notifyAll when Insertion just to 
-				  else
-				  {
-					  try {
-						  System.out.println("waiting for timing out");
 
-						q.wait(q.peek()-System.currentTimeMillis());
-					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
+	private void init() {
+		Runnable r = () -> {
+			while (true) {
+				synchronized (q) {
+					// If q is empty wait for an insertion
+					if (q.isEmpty()) {
+						try {
+							System.out.println("waiting for insertion");
+							q.wait();
+						} catch (InterruptedException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
 					}
-				  }
-			  	}
+					// if q has a element check if the alarm time has come
+					if (q.peek() - System.currentTimeMillis() <= 0) {
+						long t = q.poll();
+						System.out.println("Current Time: " + System.currentTimeMillis() + "Scheduled Time: " + t);
+					}
+					// if alarm time is not passed put a wait on the top element
+					// Note: I have added notifyAll when Insertion just to
+					else {
+						try {
+							System.out.println("waiting for timing out");
+
+							q.wait(q.peek() - System.currentTimeMillis());
+						} catch (InterruptedException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					}
+				}
 			}
 		};
 		Thread t1 = new Thread(r);
-		//t1.setDaemon(true);
 		t1.start();
-		
-		// TODO Auto-generated method stub
-		
+
 	}
+
 	public void scheduleAlarm(int delay) {
 		synchronized (q) {
-			long t = System.currentTimeMillis()+delay;
-			//We should Notify Only if the new scheduled Time is less than the top time
-			boolean shouldNotify = (q.peek()==null)|| q.peek()>t;
+			long t = System.currentTimeMillis() + delay;
+			// We should Notify Only if the new scheduled Time is less than the top time
+			boolean shouldNotify = (q.peek() == null) || q.peek() > t;
 			q.offer(t);
-			System.out.println(t+ " added to queue");
-			if(shouldNotify) {
+			System.out.println(t + " added to queue");
+			if (shouldNotify) {
 				System.out.println("Notifying all");
 				q.notifyAll();
 			}
--- src/alarm/MainClass.java
+++ src/alarm/MainClass.java
@@ -5,19 +5,19 @@ public class MainClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Alarm alarm = new Alarm();
-		Runnable r = ()->{
-			for( int i =20000;i<30000;i=i+5000) {
+		Runnable r = () -> {
+			for (int i = 20000; i < 30000; i = i + 5000) {
 				alarm.scheduleAlarm(i);
 			}
 		};
-			for(int i =0;i<=1;i++) {
+		for (int i = 0; i <= 1; i++) {
 			Thread t1 = new Thread(r);
 			t1.start();
-			}
-			//Thread t2 = new Thread(r);
+		}
+		// Thread t2 = new Thread(r);
 
-			//t2.start();
-			//t1.start();
+		// t2.start();
+		// t1.start();
 
 	}
 
